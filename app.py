ea9f0b9618be02fa81f98380d3cc782602d221e1
Victor Mustar <victor.mustar@gmail.com>
2025-01-05 17:43:47 +0100

    refactor: Remove code agent execution from app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
-
-agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
-
-agent.run(
-    "How many seconds would it take for a leopard at full speed to run through Pont des Arts?"
-)
